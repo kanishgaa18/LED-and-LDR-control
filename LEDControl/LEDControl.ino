32c3dfd9a4ceb583d9f9a3c1b5a8880461cf1667
kanishgaa18 <kanishgaaarunkumar@gmail.com>
2025-08-09 10:47:49 +0530

    modified

--- LEDControl/LEDControl.ino
+++ LEDControl/LEDControl.ino
@@ -1,12 +1,23 @@
 int ledPin = 13; 
 
 void setup() {
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT); 
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); 
-  delay(1000);                
-  digitalWrite(ledPin, LOW); 
-  delay(1000);                
+  if(Serial.available())
+  {
+    String s = Serial.readString();
+    if(s == "1")
+    {
+      digitalWrite(ledPin,HIGH);
+    }
+    else if (s == "0") {
+        digitalWrite(ledPin, LOW);  
+    }
+    else {
+      Serial.println("Invalid command. Send '1' or '0'.");
+    }
+  } 
 }
